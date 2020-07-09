0598dab984ff64a2e01b8931e73ed6375bac3417
trymte <trym.tengesdal@ntnu.no>
2020-07-09 22:32:56 +0200

    partially finished debugging obstacle: main functionality works good!

--- src/matlab_scripts/test_ownship_plot.m
+++ src/matlab_scripts/test_ownship_plot.m
@@ -1,17 +1,14 @@
 figure; axis equal
+hold on; grid on;
 [~, n_samples] = size(X);
 th = 0 : 0.01 : 2.01 * pi;
 x_a = 20 * cos(th); y_a = 20 * sin(th);
 x_cs = 50 * cos(th); y_cs = 50 * sin(th);
-hold on; grid on;
-plot(X(2, :), X(1, :), 'k');
+
+plot(X(2, :), X(1, :), 'k', 'Linewidth', 1.2);
+disp(size(X));
 for nwp = 1 : length(WPs(1, :))
     plot(y_a + WPs(2, nwp), x_a + WPs(1, nwp), 'c');
 end
 
 plot(WPs(2, :), WPs(1, :), 'rx', 'Linewidth', 1.2);
-
-for k = 1 : 50 : n_samples
-    plot(y_cs + X(2, k), x_cs + X(1, k), 'r');
-end
-legend('OS', 'WPs');
