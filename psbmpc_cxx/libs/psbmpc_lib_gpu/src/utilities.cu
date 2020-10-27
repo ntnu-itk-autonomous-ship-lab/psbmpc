67f990b909e11f74eb8536f53149330d7f384632
trymte <trym.tengesdal@ntnu.no>
2020-10-27 14:08:43 +0100

    floats suck. need to use doubles for pcoll estimation

---
+++ psbmpc_cxx/libs/psbmpc_lib_gpu/src/utilities.cu
@@ -0,0 +1,28 @@
+/****************************************************************************************
+*
+*  File name : utilities.cpp
+*
+*  Function  : CU file for all-purpose math functions which are used by multiple 
+*			   library files. Thus, do NOT add a function here if it belongs to one 
+*			   distinct class.
+*  
+*	           ---------------------
+*
+*  Version 1.0
+*
+*  Copyright (C) 2020 Trym Tengesdal, NTNU Trondheim. 
+*  All rights reserved.
+*
+*  Author    : Trym Tengesdal
+*
+*  Modified  : 
+*
+*****************************************************************************************/
+
+#include "psbmpc_defines.h"
+#include "utilities.cuh"
+#include "Eigen/Dense"
+#include "iostream"
+#include "fstream"
+#include <cstdlib>
+
