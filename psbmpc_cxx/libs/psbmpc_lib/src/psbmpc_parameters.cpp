8507825330ec52d360a76c5af9d67fe2b547a023
Trym <trym.tengesdal@ntnu.no>
2021-06-14 21:59:17 +0200

    small upd

--- psbmpc_cxx/libs/psbmpc_lib/src/psbmpc_parameters.cpp
+++ psbmpc_cxx/libs/psbmpc_lib/src/psbmpc_parameters.cpp
@@ -342,7 +342,7 @@ void PSBMPC_Parameters::initialize_par_limits()
 void PSBMPC_Parameters::initialize_pars()
 {
 	n_cbs = 1;
-	n_M = 2;
+	n_M = 1;
 	n_r = 7;
 
 	chi_offsets.resize(n_M);
@@ -351,10 +351,11 @@ void PSBMPC_Parameters::initialize_pars()
 	{
 		if (M == 0)
 		{
-			u_offsets[M].resize(3);
+			u_offsets[M].resize(2);
 
 			//u_offsets[M] << 1.0;
-			u_offsets[M] << 1.0, 0.5, 0.0;
+			u_offsets[M] << 1.0, 0.5;
+			//u_offsets[M] << 1.0, 0.5, 0.0;
 
 			chi_offsets[M].resize(13);
 			//chi_offsets[M] << 0.0;
@@ -371,13 +372,13 @@ void PSBMPC_Parameters::initialize_pars()
 			u_offsets[M] << 1.0, 0.5;
 			//u_offsets[M] << 1.0, 0.5, 0.0;
 
-			chi_offsets[M].resize(7);
+			chi_offsets[M].resize(13);
 			//chi_offsets[M] << 0.0;
 			//chi_offsets[M] << -30.0, 0.0, 30.0;
 			//chi_offsets[M] << -90.0, -45.0, 0.0, 45.0, 90.0;
 			//chi_offsets[M] << -90.0, -60.0, -30.0, 0.0, 30.0, 60.0, 90.0;
-			chi_offsets[M] << -45.0, -30.0, -15.0, 0.0, 15.0, 30.0, 45.0;
-			//chi_offsets[M] << -90.0, -75.0, -60.0, -45.0, -30.0, -15.0, 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 90.0;
+			//chi_offsets[M] << -45.0, -30.0, -15.0, 0.0, 15.0, 30.0, 45.0;
+			chi_offsets[M] << -90.0, -75.0, -60.0, -45.0, -30.0, -15.0, 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 90.0;
 			chi_offsets[M] *= DEG2RAD;
 		}
 		n_cbs *= u_offsets[M].size() * chi_offsets[M].size();
@@ -395,7 +396,7 @@ void PSBMPC_Parameters::initialize_pars()
 
 	p_step = 1;
 	p_step_cpe = 2;
-	p_step_grounding = 4;
+	p_step_grounding = 2;
 	if (prediction_method == ERK1)
 	{ 
 		dt = 0.5; 
@@ -426,7 +427,7 @@ void PSBMPC_Parameters::initialize_pars()
 	G_1 = 100.0; 
 	G_2 = 5.0;
 	G_3 = 0.3;
-	G_4 = 0.015;
+	G_4 = 0.05;
 
 	epsilon_rdp = 2.0;
 
