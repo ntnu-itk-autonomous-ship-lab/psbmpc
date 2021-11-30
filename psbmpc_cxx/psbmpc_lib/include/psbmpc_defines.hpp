d9f625117209440ad41125bb5b9531b2c40f6658
Trym <trym.tengesdal@ntnu.no>
2021-11-30 19:32:15 +0100

    upd defines to reflect mA experiment

--- psbmpc_cxx/psbmpc_lib/include/psbmpc_defines.hpp
+++ psbmpc_cxx/psbmpc_lib/include/psbmpc_defines.hpp
@@ -43,17 +43,17 @@
 #define DEG2RAD M_PI / 180.0f
 #define RAD2DEG 180.0f / M_PI
 
-#define MAX_N_M 4                       // Max number of avoidance maneuvers allowable for the PSBMPC
+#define MAX_N_M 3 // Max number of avoidance maneuvers allowable for the PSBMPC
 
-#define MAX_N_SAMPLES 400               // Max number of samples for trajectories and such allowable on the device
+#define MAX_N_SAMPLES 400 // Max number of samples for trajectories and such allowable on the device
 
-#define MAX_N_WPS 8                     // Max number of waypoints for the waypoint matrix allowable on the device
+#define MAX_N_WPS 8 // Max number of waypoints for the waypoint matrix allowable on the device
 
-#define MAX_N_DO 3                      // Max number of dynamic obstacles allowable on the device
-#define MAX_N_PS 7                      // Max number of prediction scenarios allowable for dynamic obstacle trajectory matrices on the device
+#define MAX_N_DO 3 // Max number of dynamic obstacles allowable on the device
+#define MAX_N_PS 7 // Max number of prediction scenarios allowable for dynamic obstacle trajectory matrices on the device
 
-#define MAX_N_SEG_SAMPLES 4             // Max number of samples in segment considered in the CPE method MCSKF4D
-#define MAX_N_CPE_SAMPLES 700          // Max number of samples allowable to draw in the CPE.
+#define MAX_N_SEG_SAMPLES 4   // Max number of samples in segment considered in the CPE method MCSKF4D
+#define MAX_N_CPE_SAMPLES 700 // Max number of samples allowable to draw in the CPE.
 
-#define MAX_N_SO 5                      // Max number of static obstacles/polygons allowable on the device
-#define MAX_N_VERTICES 3000             // Max number of vertices in a polygon allowable on the device
+#define MAX_N_SO 10         // Max number of static obstacles/polygons allowable on the device
+#define MAX_N_VERTICES 2000 // Max number of vertices in a polygon allowable on the device
